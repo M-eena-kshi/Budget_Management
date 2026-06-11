bd4835cce15c59f60bd4be24f09b79b83c9f4bd7
Meenakshi Choudhary <meenakshichoudhary@Meenakshis-MacBook-Air.local>
2026-06-11 17:22:04 +0530

    update expense controller and routes

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getExpenses, createExpense, deleteExpense } = require('../controllers/expenseController');
+const { getExpenses, createExpense, deleteExpense, deleteAllExpenses } = require('../controllers/expenseController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.use(protect); // Protect all routes
 
 router.route('/')
   .get(getExpenses)
-  .post(createExpense);
+  .post(createExpense)
+  .delete(deleteAllExpenses);
 
 router.route('/:id')
   .delete(deleteExpense);
